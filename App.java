58f96a27ba0fe0b3b56afe59aab59327e23cf15b
Tiffany Warren <twarre30@gmail.com>
2022-05-23 20:49:46 -0500

    add solution

--- App.java
+++ App.java
@@ -1,6 +1,25 @@
 class App {
   public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+    int vehicleIdNumber = 12566920;
+    System.out.println(vehicleIdNumber);
+
+    String vehicleMakeModel = "Ford Fusion";
+    System.out.println(vehicleMakeModel);
+
+    String vehicleColor = "Black";
+    System.out.println(vehicleColor);
+
+    boolean towingPackageIncluded = false;
+    System.out.println(towingPackageIncluded);
+
+    Float odometerReading = 40125.5f;
+    System.out.println(odometerReading);
+
+    Float vehiclePrice = 35500.00f;
+    System.out.println(vehiclePrice);
+
+    char qualityRating = 'A';
+    System.out.println(qualityRating);
+    
   }
 }
